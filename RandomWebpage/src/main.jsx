21d1ab7f79358429b131be76d7d441a30127a503
BubaiShil <bubaishil56@gmail.com>
2024-07-07 01:18:20 +0530

    sets the routes

---
+++ RandomWebpage/src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter,
+ createRoutesFromElements,
+ Route, RouterProvider } from 'react-router-dom'
+import Layout from "./Components/Layout/Layout"
+import './index.css'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Home from './Components/Home/Home'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
