1efad2a9643b08532cbf1bcdb3e5d00df57c4eaf
BubaiShil <bubaishil56@gmail.com>
2024-08-19 22:52:49 +0530

    added album bg color and added nav arrow func

--- SpotifyClone/src/Components/DisplayItem.jsx
+++ SpotifyClone/src/Components/DisplayItem.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom"
+import React, { useRef } from 'react'
+import { Route, Routes, useLocation } from "react-router-dom"
 import Home from './Home'
+import AlbumData from './AlbumData'
+import { albumsData } from '../assets/assets'
+import { useEffect } from 'react'
 
 const DisplayItem = () => {
+
+    const disref = useRef()
+    //console.log(disref);
+    const location = useLocation()
+    //console.log(location);
+    const atAlbum = location.pathname.includes('album')
+    //console.log(atAlbum);
+    const albumId = atAlbum ? location.pathname.slice(-1) : ""
+    //console.log(albumId);
+    const bgcolor = albumsData[Number(albumId)].bgColor
+    //console.log(bgcolor);
+    
+
+    useEffect(() => {  
+        atAlbum ? disref.current.style.background = `linear-gradient(${bgcolor},#121212)` : disref.current.style.background = `#121212`     
+    })
+
+
+
     return (
-        <div className='w-[100%] m-2 px-6 pt-4 text-white rounded bg-[#121212] flex flex-col lg:w-[75%] overflow-auto scrollbar-hide lg:ml-0'>
+        <div ref={disref} className='w-[100%] m-2 px-6 pt-4 text-white rounded bg-[#121212] flex flex-col lg:w-[75%] overflow-auto scrollbar-hide lg:ml-0'>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/album/:id' element={<AlbumData />} />
             </Routes>
         </div>
     )
--- SpotifyClone/src/Components/Navbar.jsx
+++ SpotifyClone/src/Components/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = () => {
+
+    //const [notAllowed, setNotAllowed] = useState(1)
+    const navigate = useNavigate()
+
     return (
         <>
             <div className='text-white w-full h-[10%]  flex items-center justify-between'>
                 <div className='flex items-center gap-3'>
-                    <img className='w-8 p-2 bg-black cursor-not-allowed rounded-full' src={assets.arrow_left} alt="" />
-                    <img className='w-8 p-2 bg-black cursor-not-allowed rounded-full' src={assets.arrow_right} alt="" />
+                    <img onClick={()=>navigate(-1)} className='w-8 p-2 bg-black rounded-full'  src={assets.arrow_left} alt="" />
+                    <img onClick={()=>navigate(1)} className='w-8 p-2 bg-black rounded-full' src={assets.arrow_right} alt="" />
                 </div>
 
                 <div className='flex gap-9 px-5 font-semibold items-center transition duration-150 ease-in-out '>
