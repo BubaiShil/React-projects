1eb0eb843b52a7282ee1653cc2a4b07222ef40fe
BubaiShil <bubaishil56@gmail.com>
2024-11-20 23:22:37 +0530

    setted up the sales page files

---
+++ AdminDashboard/src/Components/SalesEl/SalesAreaChrt.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SalesAreaChrt = () => {
+  return (
+    <div>SalesAreaChrt</div>
+  )
+}
+
+export default SalesAreaChrt
---
+++ AdminDashboard/src/Components/SalesEl/SalesBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SalesBar = () => {
+  return (
+    <div>SalesBar</div>
+  )
+}
+
+export default SalesBar
